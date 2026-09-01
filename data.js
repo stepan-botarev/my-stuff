4cdc9286b86ec278d906653b91397046071bfe26
stepan-botarev <botarevstepan@proton.me>
2026-09-01 18:27:20 +0200

    Обновление содержимого сайта через админку

--- data.js
+++ data.js
@@ -10,21 +10,21 @@ const WHATSAPP_NUMBER = "4917623657623";
 const PRODUCTS = [
   {
     "id": 1,
-    "price": "7 €",
+    "price": "7 ",
     "reserved": false,
     "images": [
       "images/item1-1788278718433-n8gn.png",
       "images/item1-1788278721055-x4nb.png"
     ],
     "title": {
-      "ru": "Retro Belt Bag with Van Gogh Print",
-      "en": "Vinyl Record Player",
-      "de": "Bauchtasche Männer / Damen / Retro mit Van-Gogh-Print",
+      "ru": "Поясная ретро-сумка с принтом в стиле Ван Гога",
+      "en": "Retro Belt Bag with Van Gogh Print",
+      "de": "Bauchtasche mit Van-Gogh-Print",
       "zh": "黑胶唱片机"
     },
     "desc": {
-      "ru": "A little 90s vibe, a little flea market, a little indie 🌙\nBlack adjustable strap, one zippered compartment. Fits a phone, passport or wallet, keys and various small essentials.\n\nGood condition, carefully used.\nMeet-up at an U-/S-Bahn station in central Hamburg or the surrounding central area. Shipping also possible 🌻",
-      "en": "Fully working, warm sound. Small scratch on the lid, doesn't affect playback.",
+      "ru": "Немного вайба 90-х, немного барахолки, немного инди 🌙\n\nЧёрный регулируемый ремень, одно отделение на молнии. Помещаются телефон, паспорт или кошелёк, ключи и всякая мелочь.\n\nХорошее состояние, носилась аккуратно.\nПередача на станции U-/S-Bahn в центральной части Гамбурга и в разумных пределах вокруг центра 🌻",
+      "en": "A little 90s vibe, a little flea market, a little indie 🌙\nBlack adjustable strap, one zippered compartment. Fits a phone, passport or wallet, keys and various small essentials.\n\nGood condition, carefully used.\nMeet-up at an U-/S-Bahn station in central Hamburg or the surrounding central area. Shipping also possible 🌻",
       "de": "Ein bisschen 90er-Vibe, ein bisschen Flohmarkt, ein bisschen Indie 🌙\nSchwarzer verstellbarer Gurt, ein Fach mit Reißverschluss. Platz für Handy, Reisepass oder Portemonnaie, Schlüssel und allerlei Kleinkram.\n\nGuter Zustand, sorgfältig getragen.\nÜbergabe an einer U-Bahn-Station im erweiterten Hamburger Zentrum. Versand auch möglich 🌻",
       "zh": "功能完好,音色温暖。盖子上有小划痕,不影响播放。"
     }
