1db2fac8ea9e35d518fb73b826387ae0b31778db
stepan-botarev <botarevstepan@proton.me>
2026-09-01 18:32:13 +0200

    Обновление содержимого сайта через админку

--- data.js
+++ data.js
@@ -20,13 +20,13 @@ const PRODUCTS = [
       "ru": "Поясная ретро-сумка с принтом в стиле Ван Гога",
       "en": "Retro Belt Bag with Van Gogh Print",
       "de": "Bauchtasche mit Van-Gogh-Print",
-      "zh": "黑胶唱片机"
+      "zh": "男女通用复古腰包 / 胸包｜梵高风格印花"
     },
     "desc": {
       "ru": "Немного вайба 90-х, немного барахолки, немного инди 🌙\n\nЧёрный регулируемый ремень, одно отделение на молнии. Помещаются телефон, паспорт или кошелёк, ключи и всякая мелочь.\n\nХорошее состояние, носилась аккуратно.\nПередача на станции U-/S-Bahn в центральной части Гамбурга и в разумных пределах вокруг центра 🌻",
       "en": "A little 90s vibe, a little flea market, a little indie 🌙\nBlack adjustable strap, one zippered compartment. Fits a phone, passport or wallet, keys and various small essentials.\n\nGood condition, carefully used.\nMeet-up at an U-/S-Bahn station in central Hamburg or the surrounding central area. Shipping also possible 🌻",
       "de": "Ein bisschen 90er-Vibe, ein bisschen Flohmarkt, ein bisschen Indie 🌙\nSchwarzer verstellbarer Gurt, ein Fach mit Reißverschluss. Platz für Handy, Reisepass oder Portemonnaie, Schlüssel und allerlei Kleinkram.\n\nGuter Zustand, sorgfältig getragen.\nÜbergabe an einer U-Bahn-Station im erweiterten Hamburger Zentrum. Versand auch möglich 🌻",
-      "zh": "功能完好,音色温暖。盖子上有小划痕,不影响播放。"
+      "zh": "一点90年代复古感，一点跳蚤市场风，一点独立音乐气质 🌙\n黑色可调节肩带，一个拉链夹层。可以放手机、护照或钱包、钥匙以及其他随身小物。\n\n保存状态良好，使用时很爱惜。\n可在汉堡市中心及周边较中心区域的地铁/城市快铁站当面交易 🌻"
     }
   }
 ];
